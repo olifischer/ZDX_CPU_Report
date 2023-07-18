cf2063407596eae8b810d4f35e2190afccc91f23
Oliver Fischer <77964103+olifischer@users.noreply.github.com>
2023-07-18 19:34:10 +0200

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,6 @@
+your_zdx_key_sec           = "your_zdx_key_sec"
+your_zdx_key_id            = "your_zdx_key_id"
+your_aws_access_key_id     = "your_aws_access_key_id"
+your_aws_secret_access_key = "your_aws_secret_access_key"
+your_s3_bucket_name        = "your_s3_bucket_name"
+your_csv_file_name         = "your_csv_file_name"
